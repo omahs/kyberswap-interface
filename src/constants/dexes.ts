55ef13410cb027566582a5aa5c4d349afa4629f8
Danh <nguyenhoaidanh2096@gmail.com>
2023-09-20 16:17:27 +0700

    add kyber pmm

--- src/constants/dexes.ts
+++ src/constants/dexes.ts
@@ -6,33 +6,5 @@ export const KYBERSWAP_KS_DEXES_TO_UI_DEXES: { [key: string]: string | undefined
   'kyberswap-static': 'kyberswapv1', // kyberswap classic new contract -> with static fee
   'kyberswap-limit-order': 'kyberswap-limit-order',
   'kyberswap-limit-order-v2': 'kyberswap-limit-order-v2',
-}
-
-export const KYBERSWAP_UI_DEXES: {
-  [key: string]: {
-    name: string
-    id: string
-    logoURL: string
-  }
-} = {
-  'kyberswap-elastic': {
-    name: 'KyberSwap Elastic',
-    id: 'kyberswap-elastic',
-    logoURL: 'https://kyberswap.com/favicon.ico',
-  },
-  kyberswapv1: {
-    name: 'KyberSwap Classic',
-    id: 'kyberswapv1',
-    logoURL: 'https://kyberswap.com/favicon.ico',
-  },
-  'kyberswap-limit-order': {
-    name: 'KyberSwap Limit Order',
-    id: 'kyberswap-limit-order',
-    logoURL: 'https://kyberswap.com/favicon.ico',
-  },
-  'kyberswap-limit-order-v2': {
-    name: 'KyberSwap Limit Order V2',
-    id: 'kyberswap-limit-order-v2',
-    logoURL: 'https://kyberswap.com/favicon.ico',
-  },
+  'kyber-pmm': 'kyber-pmm',
 }
